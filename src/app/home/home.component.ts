dde9599b9fdf4dc79c68435dd0a81b9f7d673131
shrutanten <shrutanten.work@gmail.com>
2023-09-08 12:22:02 +0530

    Fix error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import User from '../models/user.model';
+import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  userData: User | null = null;
+  authSub: Subscription;
 
+  constructor(
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.authSub = this.authService.userEmail.subscribe((email) => {
+      this.userService.getUserViaEmail(email!).subscribe((user) => {
+        this.userData = user[0];
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  userEmail = new Subject<string | null>();
+  userEmail = new BehaviorSubject<string | null>(null);
 
   login(email: string) {
     this.userEmail.next(email);
